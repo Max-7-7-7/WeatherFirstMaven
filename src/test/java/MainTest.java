daec2845d7f958106538328e1c70ccc25a11cecf
Max <maxvovk97@gmail.com>
2023-03-27 16:15:13 +0300

    Add capability

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import Helpers.Capability;
+import org.junit.Test;
+
+public class MainTest {
+
+    Capability capability = new Capability();
+
+    @Test
+    public void firstTest(){
+
+    }
+
+}
